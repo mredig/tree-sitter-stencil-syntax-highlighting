a640e3077f824eb5d965e7c2df7c4aef8893ac75
mredig <mredig@gmail.com>
2025-09-05 00:38:56 -0500

    Initial Stencil grammar

---
+++ grammar.js
@@ -0,0 +1,48 @@
+module.exports = grammar({
+  name: 'stencil',
+
+  rules: {
+    template: $ => repeat($._content),
+
+    _content: $ => choice(
+      $.text,
+      $.expression,
+      $.statement,
+      $.comment
+    ),
+
+    // Plain text content
+    text: $ => /[^{]+|{[^{%#]/,
+
+    // {{ expression }}
+    expression: $ => seq(
+      '{{',
+      optional(/\s*/),
+      $.expression_content,
+      optional(/\s*/),
+      '}}'
+    ),
+
+    expression_content: $ => /[^}]+/,
+
+    // {% statement %}
+    statement: $ => seq(
+      '{%',
+      optional(/\s*/),
+      $.statement_content,
+      optional(/\s*/),
+      '%}'
+    ),
+
+    statement_content: $ => /[^%]+/,
+
+    // {# comment #}
+    comment: $ => seq(
+      '{#',
+      optional($.comment_content),
+      '#}'
+    ),
+
+    comment_content: $ => /[^#]*#*/,
+  }
+});
